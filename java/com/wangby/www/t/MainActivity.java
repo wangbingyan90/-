4616b9680a91e1a92869a31254f4b28fe6164e96
wangbingyan90 <1418885488@qq.com>
2017-05-21 19:36:38 +0800

    图片选择（简版）

---
+++ java/com/wangby/www/t/MainActivity.java
@@ -0,0 +1,203 @@
+package com.wangby.www.t;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private static final int CROP_PHOTO = 2;
+    private static final int REQUEST_CODE_PICK_IMAGE=3;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 6;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE2 = 7;
+    private File output;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    void initView(){
+        imageView=(ImageView)findViewById(R.id.image);
+    }
+
+    public void takePhone(View view){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+
+        }else {
+            takePhoto();
+        }
+
+    }
+
+    public void choosePhone(View view){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_CALL_PHONE2);
+
+        }else {
+            choosePhoto();
+        }
+    }
+
+    /**
+     * 拍照
+     */
+    void takePhoto(){
+        /**
+         * 最后一个参数是文件夹的名称，可以随便起
+         */
+        File file=new File(Environment.getExternalStorageDirectory(),"拍照");
+        if(!file.exists()){
+            file.mkdir();
+        }
+        /**
+         * 这里将时间作为不同照片的名称
+         */
+        output=new File(file,System.currentTimeMillis()+".jpg");
+
+        /**
+         * 如果该文件夹已经存在，则删除它，否则创建一个
+         */
+        try {
+            if (output.exists()) {
+                output.delete();
+            }
+            output.createNewFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        /**
+         * 隐式打开拍照的Activity，并且传入CROP_PHOTO常量作为拍照结束后回调的标志
+         */
+        imageUri = Uri.fromFile(output);
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, CROP_PHOTO);
+
+    }
+
+    /**
+     * 从相册选取图片
+     */
+    void choosePhoto(){
+        /**
+         * 打开选择图片的界面
+         */
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");//相片类型
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+
+    }
+
+    public void onActivityResult(int req, int res, Intent data) {
+        switch (req) {
+            /**
+             * 拍照的请求标志
+             */
+            case CROP_PHOTO:
+                if (res==RESULT_OK) {
+                    try {
+                        /**
+                         * 该uri就是照片文件夹对应的uri
+                         */
+                        Bitmap bit = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        imageView.setImageBitmap(bit);
+                    } catch (Exception e) {
+                        Toast.makeText(this,"程序崩溃",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Log.i("tag", "失败");
+                }
+
+                break;
+            /**
+             * 从相册中选取图片的请求标志
+             */
+
+            case REQUEST_CODE_PICK_IMAGE:
+                if (res == RESULT_OK) {
+                    try {
+                        /**
+                         * 该uri是上一个Activity返回的
+                         */
+                        Uri uri = data.getData();
+                        Bitmap bit = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                        imageView.setImageBitmap(bit);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        Log.d("tag",e.getMessage());
+                        Toast.makeText(this,"程序崩溃",Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Log.i("liang", "失败");
+                }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                takePhoto();
+            } else
+            {
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE2)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                choosePhoto();
+            } else
+            {
+                // Permission Denied
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
